87db986cd32e6afc9749df8182dfcc83d86aa0cb
Ruxandra Popescu <ruxandrapopescu11@gmail.com>
2025-05-19 21:05:50 +0300

    edit entry

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 
+// export default function Home() {
+//   return (
+//     <div>Hello world!</div>
+//   );
+// }
+
+
+import MainPage from "@/components/MainPage";
+
 export default function Home() {
   return (
-    <div>Hello world!</div>
+    <MainPage />
   );
 }
